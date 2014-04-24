9303a2a5b891f12f1b322b8cac006716791c1984
Frug <frugen@gmail.com>
2014-04-23 22:09:08 -0400

    First working draft. Meet basic criteria, plus add an O each cycle.

--- index.php
+++ index.php
@@ -7,10 +7,14 @@
 	<link rel="stylesheet" href="css/main.css">
 	
 	<script src="js/orbital.js" type="text/javascript"></script>
+	
 </head>
 <body>
 	
-	<div id="obital"></div>
+	<div id="orbital" class="noshow"></div>
 
+<script type="text/javascript">
+	Orbital.init(document.getElementById('orbital'), 1000, 0);
+</script>
 </body>
 </html>
--- js/orbital.js
+++ js/orbital.js
@@ -12,14 +12,28 @@
  * data and return the Orbital object with our public methods.
  */
 var Orbital = (function() {
-	
-	
+
 	var me = {},			// Public properties and methods are added to "me"
 		_position,			// Position starting at the top of the screen
 		_timer,				// SetInterval reference
 		_speed,				// Time for each position in ms, defaults to 1 second = 1000 ms
-		_orbiterElement;	// Contains the "o"
+		_orbiterElement,	// Contains the "o"
+		_position_coords;
 		
+	me.startOrbiter = function() {
+		_orbiterElement.innerHTML = 'O';
+		_orbiterElement.className = 'orb' + _position;
+	}
+	
+	me.moveOrbiter = function() {
+		_position++;
+		if (_position > 4) {
+			_position = 0;
+			_orbiterElement.innerHTML += 'O';
+		}
+		
+		_orbiterElement.className = 'orb' + _position;
+	}
 	
 	/*
 	 * public function init
@@ -30,6 +44,10 @@ var Orbital = (function() {
 		_speed = startSpeed || 1000;
 		_position = startPosition || 0;
 		_orbiterElement = element;
+		
+		me.startOrbiter();
+
+		_timer = setInterval(me.moveOrbiter, _speed);
 	};
 	
 	return me;
