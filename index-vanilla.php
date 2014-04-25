74a55720a57692be4b479277c1899c6afcd55258
Frug <frugen@gmail.com>
2014-04-25 13:47:29 -0400

    rename index->index-vanilla and index2 -> index

---
+++ index-vanilla.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>TwoPointO Programming Challenge</title>
+
+	<link rel="stylesheet" href="css/main.css">
+	
+	<script src="js/orbital.js" type="text/javascript"></script>
+	
+</head>
+<body>
+	
+	<div id="orbital" class="noshow"></div>
+
+<script type="text/javascript">
+	Orbital.init(document.getElementById('orbital'), 1000, 0);
+</script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="Orbit">
 <head>
 	<meta charset="UTF-8">
 	<title>TwoPointO Programming Challenge</title>
 
 	<link rel="stylesheet" href="css/main.css">
-	
-	<script src="js/orbital.js" type="text/javascript"></script>
-	
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.5/angular.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.5/angular-resource.min.js"></script>
+	<script src="js/orbital-angular.js" type="text/javascript"></script>
 </head>
-<body>
-	
-	<div id="orbital" class="noshow"></div>
+<body ng-controller="orbController">
 
-<script type="text/javascript">
-	Orbital.init(document.getElementById('orbital'), 1000, 0);
-</script>
+	<div square id="clock">
+		<div orbital id="orbital"></div>
+	</div>
+	
 </body>
 </html>
